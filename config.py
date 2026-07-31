fead066cd62e11ad48df7f08df83267845891441
SklTlzz <sould5485@gmail.com>
2026-07-31 10:41:49 +0300

    Обновление config.py для деплоя

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-DB_USER = "postgres"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "trading_journal"
+DB_USER = os.getenv("DB_USER")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 DB_PASS = os.getenv("DB_PASSWORD")
 
 ALL_ACCOUNTS_SUM_SIZE = 40_000  # Сумма купленных счетов (5к + 10к + 25к)
