883f0d0b3701be3aa0f82d3053da3c16299afadc
SklTlzz <sould5485@gmail.com>
2026-09-01 15:07:36 +0300

    Fix emotions section

--- app_sections/emotions.py
+++ app_sections/emotions.py
@@ -14,29 +14,29 @@ def set_emotional_section(df: pd.DataFrame) -> None:
         None - the function sets up metrics and charts and returns nothing
     """
 
-    df_with_data = df[df["mistake"] != "No data"].copy()
+    df_with_mistakes = df[(df["mistake"] != "No data") & (df["mistake"] != "No mistake")].copy()
     metric_col = st.columns(1)
-    metric_col[0].metric("Total Mistakes:", len(df_with_data))
+    metric_col[0].metric("Total Mistakes:", len(df_with_mistakes))
 
     chart_cols = st.columns(3)
 
     with chart_cols[0]:
         with st.container(border=True):
-            if df_with_data.empty:
+            if df_with_mistakes.empty:
                 st.warning("No data")
 
             st.subheader("Mistakes Count")
-            mistake_counts = df_with_data["mistake"].value_counts().reset_index()
+            mistake_counts = df_with_mistakes["mistake"].value_counts().reset_index()
 
             fig = px.bar(mistake_counts, x="count", y="mistake", orientation="h", labels={"count": "Count", "mistake": "Mistake"})
             st.plotly_chart(fig, use_container_width=True, key="bar_count_mistake")
     with chart_cols[1]:
         with st.container(border=True):
-            if df_with_data.empty:
+            if df_with_mistakes.empty:
                 st.warning("No data")
 
             st.subheader("Mistakes by Session")
-            session_data = df_with_data.groupby(["trade_session"])["mistake"].count().reset_index()
+            session_data = df_with_mistakes.groupby(["trade_session"])["mistake"].count().reset_index()
 
             fig = px.pie(
                 session_data,
@@ -53,11 +53,11 @@ def set_emotional_section(df: pd.DataFrame) -> None:
             st.plotly_chart(fig, use_container_width=True, key="pie_session_mistake")
     with chart_cols[2]:
         with st.container(border=True):
-            if df_with_data.empty:
+            if df_with_mistakes.empty:
                 st.warning("No data")
 
             st.subheader("Cost of Mistake")
-            grouped_df = df_with_data.groupby(["mistake"])["profit"].sum().reset_index()
+            grouped_df = df_with_mistakes.groupby(["mistake"])["profit"].sum().reset_index()
 
             fig = px.bar(grouped_df, x="profit", y="mistake", orientation="h", labels={"profit": "Profit", "mistake": "Mistake"})
             st.plotly_chart(fig, use_container_width=True, key="bar_price_mistake")
